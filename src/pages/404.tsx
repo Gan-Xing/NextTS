00f976097b25e37964eff13d66625dce8fde6874
Gan-Xing <599153574@qq.com>
2023-10-28 14:04:47 +0800

    增加404页面

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,11 +4,14 @@ import styles from '@/styles/404.module.css';
 function Custom404() {
 	return (
 		<Layout>
-			<div className={styles.container}>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}>
 				<div className={styles.ball}></div>
-				<h1 className={styles.text}>
-					404-页面待开发 有想法随时联系
-				</h1>
+				<h1 className={styles.text}>404-页面开发中。。。</h1>
 			</div>
 		</Layout>
 	);
