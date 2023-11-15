c7919a5ae0d58105b5633b3b4d5566d2a3043b17
Gan-Xing <599153574@qq.com>
2023-11-15 10:06:43 +0800

    修改全局布局与样式

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,9 @@ import styles from '@/styles/Footer.module.css';
 const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			<p>© 2023 My Website. All rights reserved.</p>
+			<div>
+				<p>© 2023 My Website. All rights reserved.</p>
+			</div>
 		</footer>
 	);
 };
