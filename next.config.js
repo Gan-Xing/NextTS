406549cc3e71228f16404080ca4dadf9958e397b
Gan-Xing <599153574@qq.com>
2023-11-29 20:39:53 +0800

    新增本地开发代理

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/nestapi/:path*',
+        destination: 'http://localhost:3030/api/:path*' // 指向您的 NestJS 服务器
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
